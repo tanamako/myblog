c44928bf64a2b2d5106da1db0424458a2569ff1f
tanamako <ann.raf.margiela@gmail.com>
2013-12-08 22:52:37 +0900

    octopress

---
+++ config.rb
@@ -0,0 +1,19 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/myblog/"
+http_images_path = "/myblog/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/myblog/fonts"
+css_dir = "public/myblog/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
